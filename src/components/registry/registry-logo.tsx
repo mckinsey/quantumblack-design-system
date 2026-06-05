7aa2df1a2b5d8106d6c2ed428c11fdad1c30da42
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2026-06-05 10:39:46 +0200

    Create assets folder and move images

--- src/components/registry/registry-logo.tsx
+++ src/components/registry/registry-logo.tsx
@@ -1,20 +1,17 @@
 import { cn } from '@/lib/utils';
 
-import QbLogoBlack from './qb-logo-black.svg';
-import QbLogoWhite from './qb-logo-white.svg';
-
 export function RegistryLogo({ className }: { className?: string }) {
   return (
     <span className={cn('h-8 w-8 flex-shrink-0 rounded-md', className)}>
       {/* Show black logo on light mode, white logo on dark mode */}
       <img
-        src={QbLogoBlack}
+        src="/assets/logos/qb-logo-black.svg"
         alt="qb logo"
         height={24}
         className="block h-full w-full dark:hidden"
       />
       <img
-        src={QbLogoWhite}
+        src="/assets/logos/qb-logo-white.svg"
         alt="qb logo"
         width={24}
         height={24}
