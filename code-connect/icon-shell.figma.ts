e8ef0a44a855a52698508888e7d1a0cb83a74b79
Pruthvi Prakasha <pruthvip93@outlook.com>
2026-07-02 19:09:22 +0100

    revert icon shell code connect deleltion

--- code-connect/icon-shell.figma.ts
+++ code-connect/icon-shell.figma.ts
@@ -24,9 +24,28 @@ const variant = instance.getEnum('State', {
   disabled: 'disabled',
 });
 
+const swapBySize = instance.getEnum('Size', {
+  '16': 'IconSwap-16',
+  '24': 'IconSwap-24',
+  '32': 'IconSwap-32',
+});
+
+const swaps = [swapBySize, 'IconSwap-16', 'IconSwap-24', 'IconSwap-32'];
+let icon: string | undefined;
+
+for (const name of swaps) {
+  const glyph = name ? instance.getInstanceSwap(name) : null;
+
+  if (glyph && glyph.type === 'INSTANCE') {
+    icon = glyph.name;
+    break;
+  }
+}
+
 export default {
   example: figma.code`
     <IconShell size="${size}" type="${type}" variant="${variant}">
+      ${icon ? figma.code`<Icon icon="${icon}" />` : ''}
       {/* Replace with the real icon, e.g. <Icon icon="home" /> */}
     </IconShell>
   `,
